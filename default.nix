d5450b9e3d11c412eedf31b1a44efb0ed34f1fc1
Élise Souche <elise@souche.one>
2025-07-13 13:58:50 +0200

    feat: nix packaging

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ stdenv
+, lib
+}:
+
+let
+  uuid = "arsenik-quicksettings@souche.one";
+  pname = "arsenik-quicksettings";
+  version = "0.1";
+  description = "Control Arsenik from the quicksettings";
+  link = "https://github.com/elisesouche/arsenik-quicksettings";
+in
+stdenv.mkDerivation {
+  pname = "gnome-shell-extension-${pname}";
+  version = version;
+  src = ./.;
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/gnome-shell/extensions/
+    cp -r -T . $out/share/gnome-shell/extensions/${uuid}
+    runHook postInstall
+  '';
+  meta = {
+    description = builtins.head (lib.splitString "\n" description);
+    longDescription = description;
+    homepage = link;
+    license = lib.licenses.gpl2Plus; # https://gjs.guide/extensions/review-guidelines/review-guidelines.html#licensing
+    platforms = lib.platforms.linux;
+    maintainers = [ lib.maintainers.honnip ];
+  };
+  passthru = {
+    extensionPortalSlug = pname;
+    # Store the extension's UUID, because we might need it at some places
+    extensionUuid = uuid;
+  };
+}
